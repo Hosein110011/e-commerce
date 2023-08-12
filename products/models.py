e4bb5b932f7261425e62d462cd84a5885fd22f05
hosein <immortalhosein110011@gmail.com>
2023-08-13 01:48:08 +0330

    slug field added to product

--- products/models.py
+++ products/models.py
@@ -21,12 +21,40 @@ def upload_image_path(instance, filename):
     )
 
 
+class ProductQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
+
+    def featured(self):
+        return self.filter(featured=True, active=True)
+        
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return ProductQuerySet(self.model, using=self._db)
+    
+    def all(self):
+        return self.get_queryset().active()
+    
+    def featured(self):
+        return self.get_queryset().featured()
+    
+    def get_by_id(self, id):
+        qs = self.get_queryset().filter(id=id)
+        if qs.count()==1:
+            return qs.first()
+        return None
+
 
 class Product(models.Model):
     title = models.CharField(max_length=125)
     description = models.TextField()
     image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
     price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
+    featured = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
+
+    objects = ProductManager()
 
     def __str__(self):
         return self.title
--- products/views.py
+++ products/views.py
@@ -1,14 +1,18 @@
-from typing import Any, Dict
+from typing import Any, Dict, Optional
+from django.db import models
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Product
-
+from django.http import Http404
 
 
 
 class ProductListView(ListView):
-    queryset = Product.objects.all()
     template_name = 'products/products.html'
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all()
+
 
 
 class ProductDetailView(DetailView):
@@ -19,4 +23,11 @@ class ProductDetailView(DetailView):
         context =  super(ProductDetailView, self).get_context_data(*args ,**kwargs)
         return context
     
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(id=pk)
+        if instance is None:
+            raise Http404("product doesnt exist")
+        return instance
 
