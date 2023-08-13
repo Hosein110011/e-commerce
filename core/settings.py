4a0a14fabbcec8c26f3bded15e9493bf3884379b
hosein <immortalhosein110011@gmail.com>
2023-08-13 20:26:15 +0330

    create order model

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts','home','products',
-    'search','tags','carts',
+    'search','tags','carts','order',
 ]
 
 MIDDLEWARE = [
